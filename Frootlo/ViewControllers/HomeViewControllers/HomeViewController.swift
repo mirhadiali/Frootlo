4954810067dd9650a69684366649e07df283d33b
hadi ali <hadi.kudcs@gmail.com>
2020-07-06 13:38:07 +0500

    added recommended category collectionview

--- Frootlo/ViewControllers/HomeViewControllers/HomeViewController.swift
+++ Frootlo/ViewControllers/HomeViewControllers/HomeViewController.swift
@@ -10,12 +10,21 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var fruitesCollectionView: UICollectionView!
+    @IBOutlet var categoryBtn: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        fruitesCollectionView.delegate = self
+        fruitesCollectionView.dataSource = self
     }
     
+    @IBAction func dropDownBtnTap(_ sender: Any) {
+    }
+    @IBAction func addLocationBtnTap(_ sender: Any) {
+    }
 
     /*
     // MARK: - Navigation
@@ -26,5 +35,33 @@ class HomeViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+
+}
+
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FruitCollectionViewCell", for: indexPath) as! FruitCollectionViewCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+           let width = fruitesCollectionView.frame.width
+           let height = fruitesCollectionView.frame.height
+           let cell = CGSize(width: width/3 , height: height)
+           return cell
+       }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return 3
+    }
 
 }
---
+++ Frootlo/ViewControllers/HomeViewControllers/cells/FruitCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  FruitCollectionViewCell.swift
+//  Frootlo
+//
+//  Created by BYKEA - Hadi Ali on 05/07/2020.
+//  Copyright © 2020 Frootlo. All rights reserved.
+//
+
+import UIKit
+
+class FruitCollectionViewCell: UICollectionViewCell {
+    
+}
